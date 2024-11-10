b51f83b2a7c497decdca9f4a2275fbfebdd8ef63
Alif Nafili <alifnafili@gmail.com>
2024-11-10 00:30:43 +0000

    now saves your proompts

--- src/content-script.js
+++ src/content-script.js
@@ -1,15 +1,20 @@
 import { render } from "./content/qol.js";
 import { generateRoot, injectReact } from "./content/ext-qol.jsx";
-import { handleMessages } from "./content/message.js";
-import { getAllStorage, updateSettingsToContent } from "./content/storage.js";
-import Popup from "./content/Popup.jsx";
+import { handleMessages, sendMessage } from "./content/message.js";
+import { clearStore, getAllStorage, setStore } from "./content/storage.js";
+import { changeText } from "./content/wiki-scraper.js";
+
+console.log("running")
+clearStore()
+/*
+getAllStorage().then((result) => {
+    if (result.new_text){
+        changeText(new_text)
+    }
+})
+*/
 
 const root = generateRoot()
 render(document.body, root)
 
-updateSettingsToContent()
 handleMessages()
-
-getAllStorage().then((result) =>{
-    if (result.popupVisible) {injectReact(Popup, root,{startx:300,starty:500})}
-})
