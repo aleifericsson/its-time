587f61b63bf05eaa2adfda2e42f02f52a25a8cf9
Affan <affank1020@gmail.com>
2024-11-10 09:27:09 +0000

    Updated details

--- src/content-script.js
+++ src/content-script.js
@@ -1,8 +1,5 @@
 import { render } from "./content/qol.js";
 import { generateRoot, injectReact } from "./content/ext-qol.jsx";
-import { handleMessages } from "./content/message.js";
-import { getAllStorage, updateSettingsToContent } from "./content/storage.js";
-import Popup from "./content/Popup.jsx";
 import './prompt-testing.css';
 import { handleMessages, sendMessage } from "./content/message.js";
 import { clearStore, getAllStorage, setStore } from "./content/storage.js";
